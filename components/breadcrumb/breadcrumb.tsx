5eb27e4b174b31023695ad2361a8504d3001aa30
Uthman AHMAD <uthmana.muhammad@gmail.com>
2025-10-22 21:29:48 +0300

    fix: breadcrumb fixed

--- components/breadcrumb/breadcrumb.tsx
+++ components/breadcrumb/breadcrumb.tsx
@@ -5,16 +5,17 @@ export default function Breadcrumb({ menuItem }: any) {
   if (menuItem === undefined) return null;
 
   return (
-    <div className="w-full max-h-[53px] px-6 py-4 mb-4  border-b border-dotted flex justify-between items-center">
-      <h1 className="lg:text-2xl  text-base font-sans font-semibold flex-1">
+    <div className="w-full max-h-[53px] px-6 pt-4 pb-12 mb-4 flex-wrap  border-b border-dotted flex justify-between items-center">
+      <h1 className="lg:text-2xl whitespace-nowrap  text-base font-sans font-semibold ">
         {menuItem[0]?.name}
       </h1>
 
-      <ul className="inline-flex ml-auto text-sm w-fit text-black space-x-2">
+      <ul className="inline-flex  ml-auto text-sm w-fit text-black space-x-2 overflow-x-auto">
         {menuItem?.map((item: any, idx: number) => {
           if (menuItem.length !== idx + 1) {
             return (
-              <li key={idx} className="text-blue-600">
+              <li key={idx} className="text-blue-600 whitespace-nowrap">
+                {idx > 0 ? <span>{" / "}</span> : null}
                 <Link key={item?.name} href={item?.link}>
                   {item?.name}
                 </Link>
@@ -22,7 +23,7 @@ export default function Breadcrumb({ menuItem }: any) {
             );
           } else {
             return (
-              <li key={idx} className="text-gray-500">
+              <li key={idx} className="text-gray-500 whitespace-nowrap">
                 {menuItem.length > 1 ? <span>{"/ "}</span> : null}
                 {item?.name}
               </li>
